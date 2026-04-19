bca2d42bd534ba57fa00fb28cff8a8c72c66e836
Priyank Shukla <umeshusha1974@gmail.com>
2026-04-19 12:04:47 +0530

    fix moveZeroes function to correct variable usage and syntax errors

--- easy/move_zeros.java
+++ easy/move_zeros.java
@@ -1,13 +1,13 @@
 class move_zeros {
     public void moveZeroes(int[] nums) {
         int j = 0; 
-        for(int i=0; i<arr.length; i++){
+        for(int i=0; i< nums.length; i++){
             if(nums[i]!=0){
                 int temp = nums[i];
-                nums[i]=int j;
-                int j= int temp
+                nums[i]=nums[j];
+                nums[j]=temp;
 
-                j++
+                j++;
             }
         }
     }    
