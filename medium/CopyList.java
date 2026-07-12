55a57a2ec939e52a6e9e6e41ca98e7c8e9902b16
Priyank Shukla <umeshusha1974@gmail.com>
2026-07-12 10:48:08 +0530

    implement copyRandomList method to create a deep copy of a linked list with random pointers

---
+++ medium/CopyList.java
@@ -0,0 +1,34 @@
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null ) return null;
+
+        Node curr = head;
+        while(curr != null){
+            Node copy = new Node(curr.val);
+            copy.next = curr.next;
+            curr.next = copy;
+            curr = copy.next;
+        }
+
+        curr = head;
+        while(curr != null){
+
+            if(curr.random != null){
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+        curr = head;
+        Node dummy = new Node(0);
+        Node copycurr = dummy;
+
+        while(curr != null){
+            Node copy = curr.next;
+            curr.next = copy.next;
+            copycurr.next = copy;
+            copycurr = copy;
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
