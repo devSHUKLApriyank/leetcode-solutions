f52710c2d3c23fd9c0bef7de86d2c2ac1239330e
Priyank Shukla <umeshusha1974@gmail.com>
2026-09-04 11:14:18 +0530

    implement findCenter method to determine the center of a star graph from given edges

---
+++ easy/centerOfGraph.java
@@ -0,0 +1,13 @@
+public class centerOfGraph {
+    public int findCenter(int[][] edges) {
+        
+        int a = edges[0][0];
+        int b = edges[0][1];
+
+        if(a == edges[1][0] || a == edges[1][1]){
+            return a;
+        }else{
+            return b;
+        }
+    }
+} 
