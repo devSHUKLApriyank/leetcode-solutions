92a4e1a5de687d3262857993d49a7a486af9ca99
Priyank Shukla <umeshusha1974@gmail.com>
2026-09-10 11:18:50 +0530

    implement findJudge method to determine the town judge based on trust relationships

---
+++ medium/rottenOrange.java
@@ -0,0 +1,68 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        int fresh = 0;
+
+        // Saare rotten oranges queue mein daalo
+        // aur fresh oranges count karo
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+
+                if (grid[r][c] == 2) {
+                    queue.add(new int[]{r, c});
+                }
+
+                if (grid[r][c] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        int minutes = 0;
+
+        int[][] directions = {
+            {-1, 0}, // up
+            {1, 0},  // down
+            {0, -1}, // left
+            {0, 1}   // right
+        };
+
+        while (!queue.isEmpty() && fresh > 0) {
+
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+
+                int[] current = queue.poll();
+
+                int r = current[0];
+                int c = current[1];
+
+                for (int[] dir : directions) {
+
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+
+                    if (nr >= 0 && nr < rows &&
+                        nc >= 0 && nc < cols &&
+                        grid[nr][nc] == 1) {
+
+                        grid[nr][nc] = 2;
+                        fresh--;
+
+                        queue.add(new int[]{nr, nc});
+                    }
+                }
+            }
+
+            minutes++;
+        }
+
+        return fresh == 0 ? minutes : -1;
+    }
+}
