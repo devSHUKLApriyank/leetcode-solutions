2999f1c91649f8807e5998d9f78af61f4a415f36
Priyank Shukla <umeshusha1974@gmail.com>
2026-09-19 20:04:34 +0530

    implement findCircleNum method to count the number of provinces in a graph

---
+++ medium/Provincies.java
@@ -0,0 +1,41 @@
+class Solution {
+
+    public int findCircleNum(int[][] isConnected) {
+
+        int n = isConnected.length;
+
+        boolean[] visited = new boolean[n];
+
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (!visited[i]) {
+
+                provinces++;
+
+                dfs(i, isConnected, visited);
+            }
+        }
+
+        return provinces;
+    }
+
+    private void dfs(int node,
+                     int[][] isConnected,
+                     boolean[] visited) {
+
+        visited[node] = true;
+
+        for (int neighbour = 0;
+             neighbour < isConnected.length;
+             neighbour++) {
+
+            if (isConnected[node][neighbour] == 1 &&
+                !visited[neighbour]) {
+
+                dfs(neighbour, isConnected, visited);
+            }
+        }
+    }
+}
